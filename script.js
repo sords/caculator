5de1e7ebcbefd4d85de12bc4b5016e9a473e0c56
sampath <sampathsampath0852@gmail.com>
2022-08-26 22:58:14 +0530

    made the calculator work

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
+let diplaylast = document.getElementById('lastenter');
+let displaynow = document.getElementById('nowenter');
+let enternum = '';
+let enterbefore = enternum;
+let symbol = ''
+let reepeat = 1;
+let pointchecker = 0;
+//let magic = 0;
+
+
 function add(a, b) {
     return a + b;
 }
-
 function subtract(a, b) {
     return a - b;
 }
-
 function multiply(a, b) {
-    return (a * b).toFixed(3);
+    return (a * b);
 }
-
 function division(a, b) {
-    return (a / b).toFixed(3);
+    return (a / b);
 }
-
 function percentage(a, b) {
     return (a / b) * 100
+}
+function squart(a,b) {
+    return (a**b)
 }
 
 function operate(a, operator, b) {
@@ -26,140 +35,86 @@ function operate(a, operator, b) {
         return subtract(a, b);
     }
     if (operator === '*') {
+        if(pointchecker >= 1){
+            return multiply(a, b).toFixed(3);
+        }
         return multiply(a, b);
     }
     if (operator === '/') {
+        if(point >= 1){
+            return division(a, b).toFixed(3);
+        }
         return division(a, b)
     }
     if (operator === '%') {
         return percentage(a, b)
     }
-
+    if (operator === '^') {
+        return squart(a,b)
+    }
 }
 
-let diplaylast = document.getElementById('lastenter')
-let displaynow = document.getElementById('nowenter')
-let enternum = ''
-let enterbefore = enternum;
-let symbol = ''
-let reepeat = 1;
-let cap = 0;
-let point = 0;
-let magic = 0;
-function getondisplay(num, symbol = '') {
+function getondisplay(num,symbol = '',fromequal = '') {
     if (symbol !== '')
         diplaylast.textContent = num + ' ' + symbol;
+     if(enternum.includes('.') && fromequal === 'fromequal')
+       pointchecker++;
     displaynow.textContent = num;
-    //enternum = num+''
-
 }
+
 function numberentered(value) {
-    if (magic === 1) {
-        magic--
-    }
-    if (cap > 10) {
-        return
-    }
+    // if (magic === 1) 
+    //     magic--;
     if (enternum === '0') {
         enternum = ''
     }
     if (value === '.') {
-        if (point > 1) {
+        if (pointchecker >= 1) {
             return
         }
         //point was incrementing
-        point++
+        if (enternum === '') {
+            enternum = '0' + enternum
+        }
+        pointchecker++
     }
-    // let krr = Array.from(enternum)
-    // if()
-    cap++
     enternum += value
-    console.log(enternum, 2)
     getondisplay(enternum, '')
 }
 
-function change() {
-//     let plus = '+'
-//     let minus = '-'
-//     if (enternum === '') {
-//         return
-//     }
-//     let att = Array.from(enternum)
-//     if (att[0] === '-') {
-//         enternum = att.slice(1,).join(',')
-//         enternum = plus + enternum
-//         getondisplay(enternum)
-//     } else {
-//         enternum = minus + enternum
-//         getondisplay(enternum)
-//     } let kat = Number(enternum)
-    if(Number(enternum) > 0) {
-       
-        enternum = -1*(Number(enternum))
-        enternum += '' //+enternum
-        getondisplay(enternum)
-        return
-    }
-    if(Number(enternum) < 0){
-
-        enternum = -1*(Number(enternum))
-        enternum = '+' + enternum
-        getondisplay(enternum)
-        return
-    }
-
-
-}
-
-
-function symbolenter(value='-') {
-
+function symbolenter(value = '-') {
     if (enternum === '') {
-        if (magic > 0) {
-            let book = (enterbefore)
-            console.log(book)
-            diplaylast.textContent = book + value
-        }
+        // if (magic > 0) {
+        //     let book = (enterbefore)
+        //     diplaylast.textContent = book + value
+        // }
         return
-
     }
     enterbefore = enternum
-    // let before = Array.from(diplaylast.textContent);
-    // if(reepeat > 1){
-    //     enterbefore = enternum
-    // }
-    // if(magic > 1){
-    //     magic--;
-    //     enterbefore = before.slice(0,-1).join(',')
-    //     console.log(enterbefore,'inside')
-    // }else{
-    // enterbefore = displaynow.textContent + '';
-    // console.log(enterbefore)
-    // }
+    if (enternum.includes('.')) {
+        if (pointchecker >= 1) 
+            enterbefore = enternum + '0';
+    }
     symbol = value
-
     magic++;
     diplaylast.textContent = enterbefore + value
-    point--;
+   if(pointchecker >= 1)
+      pointchecker--;
     getondisplay(enternum)
     enternum = '';
-    if (reepeat === 0) {
-        return
-    }
+    if (reepeat === 0) 
+        return;
     reepeat--;
-    
 }
 let ran;
 let rose;
 
 
 function equalto() {
-    if (magic === 1) {
-        magic--
-    }
+    // if (magic === 1) 
+    //     magic--;
     let dec;
     reepeat++
-
     if (enternum === '' || reepeat > 1) {
         reepeat--
         return
@@ -167,38 +122,34 @@ function equalto() {
         ran = Number(enternum);
         rose = Number(enterbefore);
     }
-
     let man = operate(rose, symbol, ran);
+    if (pointchecker >= 1) {
+        enternum = enternum + '0'
+    }
     dec = enternum + ' ='
     diplaylast.textContent += dec;
+    if (pointchecker >= 1) {
+        pointchecker--;
+    }
     dec = enternum
     enternum = man + '';
-    getondisplay(man)
+    getondisplay(man,'','fromequal')
 }
 
 function backclear() {
     let gaw = enternum;
-    // let poinnt = gaw.split('');
     let gas;
     let lgaw
-    // if(poinnt.includes('.')){
-    //     gas = Number(gaw)/10;
-    // }else{  
-    //     gas = Math.floor(Number(gaw) / 10);  
-    // }
-
-    // let lgaw = gas + '';
-    // enternum = gas + '';
-    cap--;
-    // getondisplay(lgaw);
-    // currentOperationScreen.textContent = currentOperationScreen.textContent
-    // .toString()
-    // .slice(0, -1)
     gas = gaw.toString().slice(0, -1)
     if (gas.includes('.')) {
-        point++
+        pointchecker++
+    } //else {
+    if (pointchecker >= 1) {
+        pointchecker--
     }
-    point--
+
+    //}
+    console.log(gas, gas.includes('.'))
     if (gas === '') {
         enternum = '0'
         lgaw = '0'
@@ -206,18 +157,12 @@ function backclear() {
         enternum = gas + ''
         lgaw = gas + ''
     }
-
     getondisplay(lgaw)
 }
 
 function allclear(num) {
-    enternum = ''
-    cap = 0;
+    enternum = '';
     diplaylast.textContent = num
-    point--;
-
+    pointchecker--;
     getondisplay(num)
 }
-
-
-//check every line and use of code
